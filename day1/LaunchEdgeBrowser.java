ee7600970401d74c58bc477879d23df414ac5192
dhinudp <121571808+dhinudp@users.noreply.github.com>
2023-01-02 14:25:42 +0530

    Add files via upload

---
+++ day1/LaunchEdgeBrowser.java
@@ -0,0 +1,15 @@
+package week2.day1;
+
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class LaunchEdgeBrowser {
+
+	public static void main(String[] args) {
+		
+		
+		EdgeDriver driver = new EdgeDriver(); 
+		
+
+	}
+
+}
